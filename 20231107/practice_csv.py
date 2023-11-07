ef9bde83eeea93d593919264e1f252d145300c31
spod1n <k.spodin@gmail.com>
2023-11-07 21:30:53 +0200

    fix bug

--- 20231107/practice_csv.py
+++ 20231107/practice_csv.py
@@ -14,7 +14,7 @@ with open(file, mode='r', encoding='utf-8') as file:
     for field in csv_reader:
         if '£' in field['product_price']:
             product_price += float(field['product_price'].replace('£', '').strip())
-        elif 'P' in csv_reader:
+        elif 'P' in field['product_price']:
             product_price += float(f"0.{field['product_price'].replace('P', '').strip()}")
 
     print(f'Загальна ціна: {round(product_price, 2)}')
