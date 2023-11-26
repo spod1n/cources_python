11899b7c6d089d1e9f287c9812dc6062f515df26
spod1n <k.spodin@gmail.com>
2023-11-26 15:15:35 +0200

    update gitignore

---
+++ lessons/20231126/dataframe.py
@@ -0,0 +1,42 @@
+pip install pandas
+pip install seaborn
+pip install matplotlib
+
+
+https://colab.research.google.com/?hl=ru
+
+
+import warnings
+warnings.simplefil  ter(action='ignore', category=FutureWarning)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+%matplotlib inline
+
+sns.get_dataset_names()
+
+df = sns.load_dataset('iris')
+df.head() # показати n - перших записів, n - default 5
+df.tail()
+df.sample(n = 5)
+df.info()
+df.select_dtypes(include=['float64'])
+df.describe()
+df.isnull()
+df.isnull().sum()
+df['species']
+df['species'].values
+df['species'].value_counts()
+df['species'].value_counts(normalize= True)
+df['species'].hist();
+df['species'].unique()
+df['species'].nunique()
+df['sepal_length'] > 5.0
+df[df['sepal_length'] > 5.0]
+df[(df['sepal_length'] >= 3.15) & (df['petal_length'] <= 4.5)]
+
+# Upload tips(seaborn) - df_tips
+# EDA(head, tail, sample, hist, info, shape, unique, ...)
---
+++ lessons/20231126/practice.py
@@ -0,0 +1,36 @@
+import seaborn as sns
+import pandas
+
+import matplotlib.pyplot as plt
+
+
+def get_ds_sns(name_ds: str) -> pandas.DataFrame:
+    return sns.load_dataset(name_ds)
+
+
+def show_boxplot():
+    pass
+
+df_tips = get_ds_sns('tips')
+
+# Task 1 - Який середній розмір чайових залежно від статі клієнта?
+avg_tip_by_sex = df_tips.groupby('sex')[['total_bill', 'tip']].mean()
+print(avg_tip_by_sex)
+
+# Task 2 - Яка загальна сума рахунку залежно від дня тижня та часу доби?
+total_bill_by_day_time = df_tips.groupby(['day', 'time'])['total_bill'].sum()
+print(total_bill_by_day_time)
+
+# Task 3 - Яка залежність між розміром рахунку та курінням?
+sns.boxplot(x='smoker', y='total_bill', data=df_tips)
+plt.xlabel('Куріння')
+plt.ylabel('Розмір рахунку')
+plt.title('Залежність між розміром рахунку та курінням')
+plt.show()
+
+# Task 4 - Яка залежність між розміром чайових та курінням?
+sns.boxplot(x='smoker', y='tip', data=df_tips)
+plt.xlabel('Куріння')
+plt.ylabel('Розмір чайових')
+plt.title('Залежність між розміром чайових та курінням')
+plt.show()
