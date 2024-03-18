c7a941095e034586eaffd6884e62159f777ac5c8
spod1n <k.spodin@gmail.com>
2024-03-18 02:07:23 +0200

    end vol. 1

--- step3/homeworks/homework_2_spodinkv.py
+++ step3/homeworks/homework_2_spodinkv.py
@@ -6,3 +6,24 @@ column_names = ('sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'c
 
 iris_df = pd.read_csv(URL, header=None, names=column_names)
 iris_df.info()
+print()
+
+# 1.1 Яка середня довжина чашелистика (sepal length) для кожного виду ірису?
+mean_sepal_length = iris_df.groupby('class')['sepal_length'].mean()
+print('sepal length:', mean_sepal_length.to_markdown(), sep='\n', end='\n\n')
+
+# 1.2 Яка максимальна ширина листка (petal width) для виду “setosa”?
+max_petal_width_setosa = iris_df[iris_df['class'] == 'Iris-setosa']['petal_width'].max()
+print(f'{max_petal_width_setosa=}', end='\n\n')
+
+# 1.3 Яка розподіленість довжини листка (petal length) для всіх ірисів?
+petal_length_distribution = iris_df['petal_length'].describe()
+print('describe petal length:', petal_length_distribution.to_markdown(), sep='\n', end='\n\n')
+
+# 2.1 Створіть новий DataFrame, в якому будуть лише дані для ірисів виду “versicolor”.
+
+# 2.2 Відфільтруйте дані для ірисів з довжиною листка (petal length) більше 5.0.
+
+# 3.1 Яка середня ширина листка (petal width) для кожного виду ірису?
+# 3.2 Яка мінімальна довжина чашелистика (sepal length) для кожного виду ірису?
+# 3.3 Скільки ірисів кожного виду мають довжину листка (petal length) більше за середню довжину листка всіх ірисів?
