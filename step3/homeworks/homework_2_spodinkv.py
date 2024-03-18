07e4f19a3004b145d8b12d0949b8ee92794f9b6d
spod1n <k.spodin@gmail.com>
2024-03-18 02:24:47 +0200

    finality homework 2 (pandas)

--- step3/homeworks/homework_2_spodinkv.py
+++ step3/homeworks/homework_2_spodinkv.py
@@ -26,10 +26,21 @@ versicolor_df = iris_df[iris_df['class'] == 'Iris-versicolor'].copy()
 print('iris-versicolor only:', versicolor_df.to_markdown(), sep='\n', end='\n\n')
 
 # 2.2 Відфільтруйте дані для ірисів з довжиною листка (petal length) більше 5.0.
-petal_length_df = iris_df.loc[iris_df['petal_length'] > 5.0].sort_values(by='petal_length')
+petal_length_df = iris_df.query('petal_length > 5').sort_values(by='petal_length')
 print('petal length more than 5:', petal_length_df.to_markdown(), sep='\n', end='\n\n')
 
 
 # 3.1 Яка середня ширина листка (petal width) для кожного виду ірису?
+mean_petal_width = iris_df.groupby('class')['petal_width'].mean()
+print('petal width:', mean_petal_width.to_markdown(), sep='\n', end='\n\n')
+
 # 3.2 Яка мінімальна довжина чашелистика (sepal length) для кожного виду ірису?
+min_sepal_length = iris_df.groupby('class')['sepal_length'].min()
+print('min sepal length:', min_sepal_length.to_markdown(), sep='\n', end='\n\n')
+
 # 3.3 Скільки ірисів кожного виду мають довжину листка (petal length) більше за середню довжину листка всіх ірисів?
+avg_petal_length = iris_df['petal_length'].mean()
+
+filtered_by_mean_length = iris_df.query('petal_length > @avg_petal_length')
+iris_count = filtered_by_mean_length.groupby('class').size()
+print('iris count by class (petal length > average petal length):', iris_count.to_markdown(), sep='\n', end='\n\n')
